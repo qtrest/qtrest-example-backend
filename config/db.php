c99b5e388e4dd1ab5544bcffe16bce2cb0ac94dd
Vitaliy Petrov <v31337@gmail.com>
2014-12-08 23:35:11 +0600

    db config and "Mysql gone away" error fixing

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=admin_coupon',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=admin_coupon',
     'username' => 'admin_coupon',
     'password' => 'uruPhscu77Vy3hUA',
     'charset' => 'utf8',
