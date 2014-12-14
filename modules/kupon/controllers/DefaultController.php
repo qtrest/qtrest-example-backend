0d5ebd49502436170fef509d25e4197aa47d8ae2
Vitaliy Petrov <v31337@gmail.com>
2014-12-14 23:23:40 +0600

    add

--- modules/kupon/controllers/DefaultController.php
+++ modules/kupon/controllers/DefaultController.php
@@ -4,6 +4,10 @@ namespace app\modules\kupon\controllers;
 
 use yii\web\Controller;
 
+use app\modules\kupon\parser\ChocolifeApi as ChocolifeApi;
+use app\modules\kupon\parser\BlizzardApi as BlizzardApi;
+use app\modules\kupon\parser\KupiKuponApi as KupiKuponApi;
+
 class DefaultController extends Controller
 {
     public function actionIndex()
@@ -11,20 +15,23 @@ class DefaultController extends Controller
         return $this->render('index');
     }
 
-    public function actionFetchAll($pass)
+    public function actionFetchall($pass)
     {
         if ($pass == 'kafeg') {
             $chocolife = new ChocolifeApi();
             $chocolife->initData();
             $chocolife->fetchAllCities();
+            echo 'ChocolifeApi Success!<br/>';
 
             $blizzard = new BlizzardApi();
             $blizzard->initData();
             $blizzard->fetchAllCities();
+            echo 'BlizzardApi Success!<br/>';
 
             $kupiKupon = new KupiKuponApi();
             $kupiKupon->initData();
             $kupiKupon->fetchAllCities();
+            echo 'KupiKuponApi Success!<br/>';
         } else {
             echo 'Access denied!';
         }
