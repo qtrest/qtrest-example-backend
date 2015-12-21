25a6fe632b6c82377f8bd30ab14c9c3773bc0845
Vitaliy Petrov <v31337@gmail.com>
2015-12-21 23:52:50 +0600

    fix empty imagelist

--- views/coupon/view.php
+++ views/coupon/view.php
@@ -53,7 +53,7 @@ $features =  (trim($model->features) > '') ? preg_replace('/href="(?!http:\/\/)(
 $features = preg_replace('/src="(?!http:\/\/)([^"]+)"/', "src=\"".$model->sourceService->serviceBaseUrl."/\\1\"", $features);
 
 //images for carousel
-if (trim($images) != 'empty') {}
+if (trim($images) != 'empty') {
     $images = explode (",", $model->imagesLinks);
     foreach ($images as &$image) {
         $image = "<center><img src=\"".(substr_count($image, 'http') > 0 ? trim(($image)) :($serviceBaseUrl . '/' . trim($image)))."\" alt=\"".$serviceName." - ". $model->shortDescription ."\"/></center>";
