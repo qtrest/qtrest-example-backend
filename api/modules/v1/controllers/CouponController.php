f12fe428d59caf452286e8c58e8b75bd7aaf987e
Vitaliy Petrov <v31337@gmail.com>
2016-01-23 01:18:20 +0600

    Api pageSizeLimit

--- api/modules/v1/controllers/CouponController.php
+++ api/modules/v1/controllers/CouponController.php
@@ -63,6 +63,9 @@ class CouponController extends ActiveController
             'modelClass'  => $this->modelClass,
             'checkAccess' => [$this, 'checkAccess'],
             'params'      => \Yii::$app->request->get(),
+            'pagination' => [
+                $pageSizeLimit = [1, 2500]
+            ],
             //'prepareDataProvider' => [$this, 'prepareDataProvider']
         ];
 
